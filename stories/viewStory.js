1435b8d8ecd2aa9fb777c49f559fad5fe642fa7e
Greg Fodor <gfodor@gmail.com>
2018-05-26 15:23:45 -0700

    Initial commit

---
+++ stories/viewStory.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("View", module).add("base-view", () => {
+  //const props = { size: { width: 80, height: 80 } };
+  return <div>Hello</div>;
+});
